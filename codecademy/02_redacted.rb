df3c1c76c387104079f2bfb33909abbd50258c41
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-12-27 22:32:59 +0000

    Initial commit

---
+++ codecademy/02_redacted.rb
@@ -0,0 +1,14 @@
+puts "Original Text"
+text = gets.chomp
+puts "Redacted Word"
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word == redact
+    print "REDACTED "
+  else
+    print word + " "
+  end
+end
