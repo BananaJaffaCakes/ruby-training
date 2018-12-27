4ec14f3b6a6a750b5edeae145f7cf9d4c9fba550
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-12-27 23:26:59 +0000

    Removed case sensitive matching

--- codecademy/02_redacted.rb
+++ codecademy/02_redacted.rb
@@ -1,16 +1,21 @@
-#What could you do to make sure your redactor redacts a word regardless of whether it's upper case or lower case?
+#COMPLETED-What could you do to make sure your redactor redacts a word regardless of whether it's upper case or lower case?
 #How could you make your program take multiple, separate words to REDACT?
 #How might you make a new redacted string and save it as a variable, rather than just printing it to the console?
+
+#ruby .\02_redacted.rb
 
 puts "Original Text"
 text = gets.chomp
 puts "Redacted Word"
 redact = gets.chomp
+redact.downcase!
 
 words = text.split(" ")
 
 words.each do |word|
-  if word == redact
+  downcase_word = word.downcase
+  #print "match #{downcase_word} : #{redact} print "
+  if downcase_word == redact
     print "REDACTED "
   else
     print word + " "
