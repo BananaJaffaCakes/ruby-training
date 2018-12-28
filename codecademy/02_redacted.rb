fd38fd95bcb550882abc08c8b3146a95a9c5f84c
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2018-12-28 20:56:14 +0000

    String with redacted words saved to variable

--- codecademy/02_redacted.rb
+++ codecademy/02_redacted.rb
@@ -1,6 +1,7 @@
 #COMPLETED-What could you do to make sure your redactor redacts a word regardless of whether it's upper case or lower case?
 #COMPLETED-How could you make your program take multiple, separate words to REDACT?
-#How might you make a new redacted string and save it as a variable, rather than just printing it to the console?
+#COMPLETED-How might you make a new redacted string and save it as a variable, rather than just printing it to the console?
+#TODO Remove initial space from updated_words string
 
 #ruby .\02_redacted.rb
 
@@ -15,15 +16,18 @@ redact_words = redact.split(" ")
 
 
 puts "**Updated Words"
+updated_words = ""
+
 words.each do |word|
   downcase_word = word.downcase
   #print "match #{downcase_word} : #{redact} print "
-  
+
   print_value = 0
 
   redact_words.each do |redact_word|
     if downcase_word == redact_word
       print "REDACTED "
+      updated_words = "#{updated_words} REDACTED"
       print_value = 1
     end
   break if print_value == 1
@@ -31,5 +35,8 @@ words.each do |word|
 
   if print_value == 0
     print word + " "
+    updated_words = "#{updated_words} #{word}"
   end
 end
+puts ""
+print updated_words
