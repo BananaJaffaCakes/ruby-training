fe1a3d1dc67fd703c001094ff8f5cdde5c67d587
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2019-01-01 17:22:05 +0000

    Further comments added

--- codecademy/03_histogram.rb
+++ codecademy/03_histogram.rb
@@ -1,10 +1,16 @@
+#ruby .\03_histogram.rb
+
 puts "Text:"
 text = gets.chomp
 words = text.split(" ")
 
+#hash constructor notation
 frequencies = Hash.new(0) #creates a new empty hash where values for items are defaulted to zero
 
-words.each {|item| frequencies[item] += 1}
+#hash literal notation
+#frequencies = {"i1" => 1, "i2" => 2}
+
+words.each {|item| frequencies[item] += 1} #example of a code block
 
 frequencies = frequencies.sort_by {|item, count| count}
 frequencies.reverse!
--- codecademy/04_libraryordering.rb
+++ codecademy/04_libraryordering.rb
@@ -1,14 +1,14 @@
 def alphabetize(arr, rev=false) #rev is set to false if arguement is missing when method called
-    arr.sort!
-    if rev
-      arr.reverse!
-    else
-      return arr
-    end
+  arr.sort!
+  if rev
+    arr.reverse!
+  else
+    return arr
   end
+end
   
-  numbers = [1,4,2,6,20,11]
+numbers = [1,4,2,6,20,11]
   
-  alphabetize(numbers, true)
+alphabetize(numbers, true)
   
-  puts numbers
+puts numbers
