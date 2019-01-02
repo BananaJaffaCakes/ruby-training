4787875ed30d58b2082d588c4e78be076c90fbc0
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2019-01-02 22:40:07 +0000

    CRUD features added

--- codecademy/05_movies.rb
+++ codecademy/05_movies.rb
@@ -1,21 +1,50 @@
 movies = {
-    matrix: 10,
-    peter_rabbit: 6,
-    soccer: 2
-    }
-  
-  puts "User Input"
-  choice = gets.chomp
-  
-  case choice
-    when "add"
-      puts "Added!"
-    when "update"
-      puts "Updated!"
-    when "display"
-      puts "Movies!"
-    when "delete"
-      puts "Deleted!"
+  matrix: 10,
+  peter_rabbit: 6,
+  soccer: 2
+  }
+
+puts "User Input"
+choice = gets.chomp
+
+case choice
+  when "add"
+    puts "Movie title:"
+    title = gets.chomp
+    puts "Rating:"
+    rating = gets.chomp
+    if movies[title.to_sym] == nil #NEW
+      movies[title.to_sym] = rating.to_i #NEW
+      puts "Movie added!"
     else
-      puts "Error!"
-  end
+      puts "Movie already exists"
+    end
+    print movies
+  when "update"
+    puts "Movie title:"
+    title = gets.chomp
+    if movies[title.to_sym] == nil
+      puts "Movie does not exist"
+    else
+      puts "New rating:"
+      rating = gets.chomp
+      movies[title.to_sym] = rating.to_i
+      puts "Rating updated!"
+    end
+    print movies
+  when "display"
+    movies.each {|k, v| puts "#{k}: #{v}"}
+  when "delete"
+    puts "Movie title:"
+    title = gets.chomp
+    if movies[title.to_sym] == nil
+      puts "Movie does not exist"
+    else
+      movies.delete(title.to_sym) #NEW
+      puts "Movie deleted!"
+    end
+    print movies
+  else
+    puts "Error!"
+end
+  
