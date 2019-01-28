adb257822e083ccce5a5880e3605c852f3f94442
gitUser01 <44544977+BananaJaffaCakes@users.noreply.github.com>
2019-01-28 22:50:49 +0000

    Added deposit method

--- codecademy/08_banking.rb
+++ codecademy/08_banking.rb
@@ -1,8 +1,10 @@
 =begin
 Class improvements
-Include a deposit method that lets users add money to their accounts
-Include error checking that prevents users from overdrawing their accounts
+Completed - Include a deposit method that lets users add money to their accounts
+In-progress - Include error checking that prevents users from overdrawing their accounts
 Create CheckingAccounts or SavingsAccounts that inherit from Account
+
+ruby .\08_banking.rb
 =end
 
 class Account
@@ -19,9 +21,20 @@ class Account
     end
     
     def withdraw(pin_number, amount)
-      if pin_number == pin
+      if pin_number == pin and (@balance - amount) >= 0
         @balance -= amount
-        puts "Withdrew #{amount}. New balance: $#{@balance}"
+        puts "Withdrew $#{amount}. New balance: $#{@balance}"
+      elsif pin_number == pin and (@balance - amount) < 0
+        puts "Withdrawal amount of $#{amount} exceeds balance: $#{@balance}"
+      else
+        puts pin_error
+      end
+    end
+
+    def deposit(pin_number, amount)
+      if pin_number == pin
+        @balance += amount
+        puts "Deposited $#{amount}. New balance: $#{@balance}"
       else
         puts pin_error
       end
@@ -39,4 +52,9 @@ class Account
    
   end
   
-  checking_account = Account.new("checking_account", 100_000)
+checking_account = Account.new("checking_account", 100_000)
+checking_account.display_balance(1111)
+checking_account.display_balance(1234)
+checking_account.withdraw(1234, 1_000_000)
+checking_account.deposit(1234, 10)
+
